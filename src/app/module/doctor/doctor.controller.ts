e6e57a97057ebf21874f031e37d25b86e8247cac
Topu Rayhan <topurayhantipu@gmail.com>
2026-08-26 12:55:06 +0600

    update to doctor controller

--- src/app/module/doctor/doctor.controller.ts
+++ src/app/module/doctor/doctor.controller.ts
@@ -14,6 +14,7 @@ const applyAsDoctor = catchAsync(async (req: Request, res: Response) => {
   const zodValidationResult = ApplyAsDoctorValidationZodSchema.safeParse(
     JSON.parse(req.body.data),
   );
+  // fdsfjkjdf
 
   if (!zodValidationResult.success) {
     throw new Error(zodValidationResult.error.issues[0].message);
