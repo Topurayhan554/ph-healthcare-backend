d582e66c666fe948d3168b9b32ab6533ed504fd2
Topu Rayhan <topurayhantipu@gmail.com>
2026-08-26 12:47:07 +0600

    implement doctor application with document uploads and validation

--- src/app/module/doctor/doctor.controller.ts
+++ src/app/module/doctor/doctor.controller.ts
@@ -1,16 +1,35 @@
-import { Request, Response } from "express";
-import { catchAsync } from "../../utils/catchAsync";
-import { DoctorServices } from "./doctor.service";
+import type { Request, Response } from "express";
 import httpStatus from "http-status";
+import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
+import { DoctorServices } from "./doctor.service";
+import { ApplyAsDoctorValidationZodSchema } from "./doctor.validation";
 
 const applyAsDoctor = catchAsync(async (req: Request, res: Response) => {
-  const result = await DoctorServices.applyAsDoctor();
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  console.log({ files });
+  const resume = files?.["resume"] ? files["resume"][0] : null;
+  const additionalFiles = files?.["additionalFiles"] || [];
 
+  const zodValidationResult = ApplyAsDoctorValidationZodSchema.safeParse(
+    JSON.parse(req.body.data),
+  );
+
+  if (!zodValidationResult.success) {
+    throw new Error(zodValidationResult.error.issues[0].message);
+  }
+
+  const payload = zodValidationResult.data;
+
+  const result = await DoctorServices.applyAsDoctor(
+    payload,
+    resume,
+    additionalFiles,
+  );
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: "Applied As Doctor Successfully",
+    message: "Applied As Doctor Successfuly",
     data: result,
   });
 });
